a3539ea077e682a15b9676f286f5e67734dd71df
munoztd0 <david.munoztord@mailbox.org>
2024-09-01 13:22:06 +0900

    feat: :sparkles: introducing importing feature

---
+++ R/mod_import_table.R
@@ -0,0 +1,81 @@
+#' import_table UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_import_table_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    fileInput(ns("file"), "Choose CSV or Excel File",
+              accept = c(".csv", ".xlsx", ".xls")),
+    textInput(ns("table_name"), "Enter table name for import:"),
+    actionButton(ns("import"), "Import File"),
+    verbatimTextOutput(ns("import_status"))
+  )
+}
+
+#' import_table Server Functions
+#'
+#' @noRd 
+#'
+#' @importFrom shiny moduleServer reactiveVal
+#' @importFrom  readxl read_excel
+#' @importFrom readr read_csv
+
+# Server Function
+mod_import_table_server <- function(id, table_names) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    import_status <- reactiveVal()
+
+    observeEvent(input$import, {
+      req(input$file, input$table_name)
+      
+      tryCatch({
+        # Read the file
+        if (tools::file_ext(input$file$datapath) == "csv") {
+          df <- readr::read_csv(input$file$datapath)
+        } else {
+          df <- readxl::read_excel(input$file$datapath)
+        }
+        
+        # Validate table name
+        table_name <- input$table_name
+        if (!grepl("^[a-zA-Z][a-zA-Z0-9_]*$", table_name)) {
+          stop("Invalid table name. Use only letters, numbers, and underscores, starting with a letter.")
+        }
+        if (tolower(table_name) %in% tolower(table_names())) {
+          stop("Table name already exists. Please choose a different name.")
+        }
+        
+        # Get database connection
+        conn <- golem::get_golem_options("conn_SQL_Lite")
+        
+        # Write to database
+        DBI::dbWriteTable(conn, table_name, df, overwrite = TRUE)
+        
+        # Update table names
+        table_names(DBI::dbListTables(conn))
+        
+        import_status("File imported successfully!")
+      }, error = function(e) {
+        import_status(paste("Error:", e$message))
+      })
+    })
+
+    output$import_status <- renderText({
+      import_status()
+    })
+  })
+}  
+    
+## To be copied in the UI
+# mod_import_table_ui("import_table_1")
+    
+## To be copied in the server
+# mod_import_table_server("import_table_1")
